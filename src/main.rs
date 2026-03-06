3b1b64c53567cd9bc9234dc13d8321b7731163b5
notnotnull <jesteralex@hotmail.com>
2026-03-06 18:56:15 -0500

    add error handling with anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
+use anyhow::{Context, Result};
 use clap::Parser;
 
+/// Read a file
 #[derive(Parser)]
 struct Cli {
-    // The path to the file to read
+    /// The path to the file to read
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
     // Handle arguments with clap
     let args = Cli::parse();
 
-    println!("path: {:?}", args.path)
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("Unable to read file `{}`", args.path.display()))?;
+
+    for line in content.lines() {
+        println!("{}", line);
+    }
+
+    Ok(())
 }
