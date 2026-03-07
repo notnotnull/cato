284e75ab1791f0018263586ec58c050473ef4cff
notnotnull <jesteralex@hotmail.com>
2026-03-07 10:24:12 -0500

    add a better write buffer and version information

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # rcat
+
+A lightweight file concatenation tool written in rust.
+
+## Usage
+
+```bash
+rcat <file>
+```
+
+```bash
+rcat --help
+```
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::io::{self, Write};
 use anyhow::{Context, Result};
 use clap::Parser;
 
-/// Read a file
+/// Command-line Interface options
 #[derive(Parser)]
+#[command(version)]
 struct Cli {
     /// The path to the file to read
     path: std::path::PathBuf,
@@ -12,11 +14,18 @@ fn main() -> Result<()> {
     // Handle arguments with clap
     let args = Cli::parse();
 
+    // Handle arguments with anyhow for more context
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("Unable to read file `{}`", args.path.display()))?;
 
+    // Get the global stdout entity
+    let stdout = io::stdout();
+    // wrap the handle in a buffer to reduce flushes
+    let mut handle = io::BufWriter::new(stdout);
+
     for line in content.lines() {
-        println!("{}", line);
+        writeln!(handle, "{}", line)
+            .with_context(|| format!("Unable to print file contents `{}`", args.path.display()))?;
     }
 
     Ok(())
