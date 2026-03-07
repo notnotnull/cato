2abdb70bef08c101902470e2b414758aab07e626
notnotnull <jesteralex@hotmail.com>
2026-03-07 11:11:05 -0500

    add multiple file concatenation

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,28 @@ use clap::Parser;
 #[command(version)]
 struct Cli {
     /// The path to the file to read
-    path: std::path::PathBuf,
+    #[arg(num_args = 0..)]
+    files: Vec<std::path::PathBuf>,
 }
 
 fn main() -> Result<()> {
     // Handle arguments with clap
     let args = Cli::parse();
 
-    // Handle arguments with anyhow for more context
-    let content = std::fs::read_to_string(&args.path)
-        .with_context(|| format!("Unable to read file `{}`", args.path.display()))?;
-
     // Get the global stdout entity
     let stdout = io::stdout();
     // wrap the handle in a buffer to reduce flushes
     let mut handle = io::BufWriter::new(stdout);
 
-    for line in content.lines() {
-        writeln!(handle, "{}", line)
-            .with_context(|| format!("Unable to print file contents `{}`", args.path.display()))?;
+    for path in args.files {
+        // Handle arguments with anyhow for more context
+        let content = std::fs::read_to_string(&path)
+            .with_context(|| format!("Unable to read file `{}`", path.display()))?;
+
+        for line in content.lines() {
+            writeln!(handle, "{}", line)
+                .with_context(|| format!("Unable to print file contents `{}`", path.display()))?;
+        }
     }
 
     Ok(())
